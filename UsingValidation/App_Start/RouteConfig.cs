87b332fa7c7f77b61a4016a0027e4b235e425013
Lance Kent <lancemagollado46@gmail.com>
2025-10-31 16:37:04 +0800

    Controller and View Page Created

--- UsingValidation/App_Start/RouteConfig.cs
+++ UsingValidation/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UsingValidation
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Validation", action = "Registration", id = UrlParameter.Optional }
             );
         }
     }
---
+++ UsingValidation/Controllers/ValidationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UsingValidation.Controllers
+{
+    public class ValidationController : Controller
+    {
+        // GET: Validation
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
